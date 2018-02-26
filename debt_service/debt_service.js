3d676b53c1bc797ef131a80a58a7a1479bdf7c78
aescherling <adam.scherling@gmail.com>
2018-02-26 15:45:40 -0800

    debt service chart tentatively complete

--- debt_service/debt_service.js
+++ debt_service/debt_service.js
@@ -1,6 +1,48 @@
-// inspiration from https://bl.ocks.org/mbostock/3885304
+// code mostly copied from https://bl.ocks.org/mbostock/3887051
 
-var myData;
+/* Initial Setup */
+
+// Constants for sizing  
+var w = window,
+    d = document,
+    e = d.documentElement,
+    g = d.getElementsByTagName('body')[0],
+    x = w.innerWidth || e.clientWidth || g.clientWidth,
+    y = w.innerHeight|| e.clientHeight|| g.clientHeight;
+
+// maintain a ratio of 2.5 width to 1 height, but scale to window. Max width of 1000.
+var width = d3.min([1000, y * 1.2, x * 0.75]);
+var height = width / 2.5;
+
+var svg = d3.select("#vis")
+    .append("svg")
+    .attr('height', height)
+    .attr('width', width);
+
+var margin = {top: 40, right: width * 0.3, bottom: 80, left: 80},
+    width = +svg.attr("width") - margin.left - margin.right,
+    height = +svg.attr("height") - margin.top - margin.bottom,
+    g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+// scale for groups
+var x0 = d3.scaleBand()
+    .rangeRound([0, width])
+    .paddingInner(0.2);
+
+// scale within groups
+var x1 = d3.scaleBand()
+    .padding(0.1);
+
+// scale for height
+var y = d3.scaleLinear()
+    .rangeRound([height, 0]);
+
+// scale for color
+var z = d3.scaleOrdinal()
+    .range(["skyblue", "steelblue"]);
+
+var axisTextSize = d3.min([d3.max([8, (height / 20)]), 15]) + "px"
+
 
 // function from https://bl.ocks.org/mbostock/7555321 for wrapping text
 function wrap(text, width) {
@@ -28,215 +70,78 @@ function wrap(text, width) {
 }
 
 
-// function to call on the data
-plotData = function(error, data, dataType) {
+
+
+
+d3.csv("debt_service.csv", function(d, i, columns) {
+  // convert data from text to numbers, divide by 1000
+  for (var i = 1, n = columns.length; i < n; ++i) d[columns[i]] = +d[columns[i]] / 1000;
+  return d;
+}, function(error, data) {
   if (error) throw error;
 
-  myData = data;
+  var keys = data.columns.slice(1);
 
-  /* Initial Setup */
+  x0.domain(data.map(function(d) { return d.Name; }));
+  x1.domain(keys).rangeRound([0, x0.bandwidth()]);
+  y.domain([0, d3.max(data, function(d) { return d3.max(keys, function(key) { return d[key]; }); })]).nice();
 
-  // Constants for sizing
-  var w = window,
-      d = document,
-      e = d.documentElement,
-      g = d.getElementsByTagName('body')[0],
-      x = w.innerWidth || e.clientWidth || g.clientWidth,
-      y = w.innerHeight|| e.clientHeight|| g.clientHeight;
-
-  // maintain a ratio of 2.5 width to 1 height, but scale to window. Max width of 1000.
-  var width = d3.min([1000, y * 1.2, x * 0.75]);
-  var height = width / 2.5;
-
-  // scale the toolbars
-  var label_size = d3.min([d3.max([12, (height / 15)]), 18]) + "px",
-      entry_size = d3.min([d3.max([10, (height / 20)]), 16]) + "px",
-      heading_size = d3.min([d3.max([16, (height / 10)]), 46]) + "px";
-  d3.selectAll('#heading').style('font-size', heading_size);
-  d3.selectAll('.toolbar_label').attr('style', "padding:0px 0px 0px 10px; font-weight: bold; font-size: " + label_size);
-  d3.selectAll('.btn-group').selectAll('.btn').attr('style', "font-size: " + entry_size);
-
-  var svg = d3.select("#vis")
-    .append("svg")
-    .attr('height', height)
-    .attr('width', width);
-
-  var graphMargin = {top: 40, right: width * 0.4, bottom: 80, left: 80},
-    graphWidth = width - graphMargin.left - graphMargin.right,
-    graphHeight = height - graphMargin.top - graphMargin.bottom;
-
-  var x = d3.scaleBand().rangeRound([0, graphWidth]).padding(0.1).paddingInner(0.4),
-      y = d3.scaleLinear().rangeRound([graphHeight, 0]);
-
-  var axisTextSize = d3.min([d3.max([8, (graphHeight / 20)]), 15]) + "px"
-
-  var graph = svg.append("g")
-      .attr("transform", "translate(" + graphMargin.left + "," + graphMargin.top + ")");
-
-    x.domain(data.map(function(d) { return d.name; }));
-    y.domain([0, 0.16]);
-
-  // most recent year of data
-  var current_year = d3.max(data.map(function(d) {return d.year}));
-
-  // draw x axis
-  graph.append("g")
-      .style('font-size', axisTextSize)
-      .attr("class", "axis axis--x")
-      .attr("transform", "translate(0," + graphHeight + ")")
-      .call(d3.axisBottom(x));
-
-  // draw y axis
-  graph.append("g")
-      .style('font-size', axisTextSize)
-      .attr("class", "axis axis--y")
-      .call(d3.axisLeft(y).ticks(10, "%"))
-      .append("text")
-      .classed('yAxisText', true)
-      .attr('x', 10)
-      .attr('y', -30)
-      .attr("dy", "0.71em")
-      .attr("fill", "#000")
-      .text("");;
-
-  /* initialize bars with zero height */
-  // total obligations (although only interest will be visible)
-  graph.selectAll('tBar')
+  g.append("g")
+    .selectAll("g")
     .data(data)
-    .enter().append('rect')
-    .attr('class', 'bar tBar')
-    .attr('x', function(d) {return x(d.name)})
-    .attr('y', graphHeight)
-    .attr('width', x.bandwidth())
-    .attr('height', 0)
-    .attr('fill', 'skyblue')
-    .on('mouseenter', showFloatingTooltip)
-    .on('mouseleave', function() {
-      graph.select('.outline').remove();
-      floating_tooltip.hideTooltip();
-    });
-
-  // bar for principal
-  graph.selectAll(".uBar")
-    .data(data)
+    .enter().append("g")
+      .attr("transform", function(d) { return "translate(" + x0(d.Name) + ",0)"; })
+    .selectAll("rect")
+    .data(function(d) { return keys.map(function(key) { return {key: key, value: d[key]}; }); })
     .enter().append("rect")
-    .attr("class", "bar uBar")
-    .attr("x", function(d) { return x(d.name); })
-    .attr("y", graphHeight)
-    .attr("width", x.bandwidth())
-    .attr("height", 0)
-    .attr('fill', 'steelblue')
-    .on('mouseenter', showFloatingTooltip)
-    .on('mouseleave', function() {
-      graph.select('.outline').remove();
-      floating_tooltip.hideTooltip();
-    });
+      .attr("x", function(d) { return x1(d.key); })
+      .attr("y", function(d) { return y(d.value); })
+      .attr("width", x1.bandwidth())
+      .attr("height", function(d) { return height - y(d.value); })
+      .attr("fill", function(d) { return z(d.key); });
 
-  // make legend
-  legend = graph.append('g');
-  legendX = width * 0.6;
-  legendY = graphHeight * 0.25
-  legendSize = 0.8 * x.bandwidth();
+  g.append("g")
+      .attr("class", "axis-x0")
+      .style('font-size', axisTextSize)
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x0));
 
-  // voter-approved debt
-  legend.append('rect')
-    .attr('x', legendX)
-    .attr('y', legendY + legendSize)
-    .attr('width', legendSize)
-    .attr('height', legendSize)
-    .attr('fill', 'skyblue');
+  g.append("g")
+      .attr("class", "axis")
+      .style('font-size', axisTextSize)
+      .call(d3.axisLeft(y).ticks(null))
+    .append("text")
+      .attr('font-size', axisTextSize)
+      .attr("x", 2)
+      .attr("y", y(y.ticks().pop()) + 0.5)
+      .attr("dy", "-2em")
+      .attr("fill", "#000")
+      .text('$ (Millions)');
 
-  legend.append('text')
-    .attr('x', legendX + 1.5 * legendSize)
-    .attr('y', legendY + 1.6 * legendSize)
-    .attr('font-size', axisTextSize)
-    .text('Interest');
+  var legend = g.append("g")
+      .attr('transform', 'translate(130,100)')
+      .attr("font-family", "sans-serif")
+      .attr("font-size", 10)
+      .attr("text-anchor", "end")
+    .selectAll("g")
+    .data(keys.slice().reverse())
+    .enter().append("g")
+      .attr("transform", function(d, i) { return "translate(0," + i * 35 + ")"; });
 
-  // non-voter-approved debt
-  legend.append('rect')
-    .attr('x', legendX)
-    .attr('y', legendY + 2.5 * legendSize)
-    .attr('width', legendSize)
-    .attr('height', legendSize)
-    .attr('fill', 'steelblue');
+  legend.append("rect")
+      .attr("x", width - 25)
+      .attr("width", 25)
+      .attr("height", 25)
+      .attr("fill", z);
 
-  legend.append('text')
-    .attr('x', legendX + 1.5 * legendSize)
-    .attr('y', legendY + 3.1 * legendSize)
-    .attr('font-size', axisTextSize)
-    .text('Principal');
-
-
-  // function for toggling to percentage view
-  function drawValues() {
-    // set the scale
-    var y = d3.scaleLinear().rangeRound([graphHeight, 0]).domain([0, 4500]);
-
-    // change the axis
-    d3.selectAll('.axis--y')
-        .call(d3.axisLeft(y).ticks(10));
-
-    d3.selectAll('.yAxisText')
-        .text('$ (Millions)');
-
-    // transition the bars
-    graph.selectAll('.tBar')
-      .transition()
-      .duration(500)
-      .attr('y', function(d) {return y(d.total / 1e6)})
-      .attr('height', function(d) {return graphHeight - y(d.total / 1e6); });
-
-    // bar for principal
-    graph.selectAll(".uBar")
-      .transition()
-      .duration(500)
-      .attr("y", function(d) { return y(d.principal / 1e6); })
-      .attr("height", function(d) { return graphHeight - y(d.principal / 1e6); });
-
-  }
-
-  // draw the bars
-  drawValues();
-
-
-
-  /* tooltip for displaying data on each item */
-  var floating_tooltip = floatingTooltip('floatingTooltip', "215px");
-
-  function showFloatingTooltip(d) {
-
-    var y = d3.scaleLinear().rangeRound([graphHeight, 0]).domain([0, 4500]);
-    var h = y(d.total / 1e6);
-
-    // outline the bar for that year
-    graph.append('rect')
-      .attr('class', 'outline')
-      .attr('x', x(d.name))
-      .attr('y', h)
-      .attr('width', x.bandwidth())
-      .attr('height', graphHeight - h)
-      .attr('fill', 'none')
-      .attr('stroke', 'black')
-      .attr('stroke-width', 2);
-
-    var content = '<span class="heading"><p style="text-align: center">' + d.name + '</p></span>' +
-                  '<table><tr><td style="padding: 0px 10px 0px 20px">Principal: </td><td style="text-align: center">' + formatAmount(d.principal) + '</td></tr>' +
-                  '<tr><td style="padding: 0px 10px 0px 20px">Interest: </td><td style="text-align: center">' + formatAmount(d.interest) + '</td></tr>' + 
-                  '<tr><td style="padding: 0px 10px 0px 20px">Total: </td><td style="text-align: center">' + formatAmount(d.total) + '</td></tr></table>';
-    // display tooltip
-    floating_tooltip.revealTooltip(content, d3.event);
-  }
+  legend.append("text")
+      .style('font-size', axisTextSize)
+      .attr("x", width - 30)
+      .attr("y", 11.5)
+      .attr("dy", "0.32em")
+      .text(function(d) { return d; });
 
   // wrap x-axis labels
-  d3.select('.axis--x').selectAll(".tick text")
-    .call(wrap, 100);
-
-}
-
-d3.csv("outstanding_debt.csv", function(d) {
-  d.name = d.Name;
-  d.principal = d.Principal * 1000;
-  d.interest = d.Interest * 1000;
-  d.total = d.Total * 1000;
-  return d;
-}, plotData);
+  d3.select('.axis-x0').selectAll(".tick text")
+    .call(wrap, 80);
+});
