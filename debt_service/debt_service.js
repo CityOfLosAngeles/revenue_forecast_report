ff65dc65eda5b2d47c82d3cbd6f391b4fdf4c968
aescherling <adam.scherling@gmail.com>
2018-02-26 14:59:51 -0800

    initial edits to index.html, copied outstanding_debt.js as debt_service.js

--- debt_service/debt_service.js
+++ debt_service/debt_service.js
@@ -1,8 +1,38 @@
 // inspiration from https://bl.ocks.org/mbostock/3885304
 
+var myData;
+
+// function from https://bl.ocks.org/mbostock/7555321 for wrapping text
+function wrap(text, width) {
+  text.each(function() {
+    var text = d3.select(this),
+        words = text.text().split(/\s+/).reverse(),
+        word,
+        line = [],
+        lineNumber = 0,
+        lineHeight = 1.1, // ems
+        y = text.attr("y"),
+        dy = parseFloat(text.attr("dy")),
+        tspan = text.text(null).append("tspan").attr("x", 0).attr("y", y).attr("dy", dy + "em");
+    while (word = words.pop()) {
+      line.push(word);
+      tspan.text(line.join(" "));
+      if (tspan.node().getComputedTextLength() > width) {
+        line.pop();
+        tspan.text(line.join(" "));
+        line = [word];
+        tspan = text.append("tspan").attr("x", 0).attr("y", y).attr("dy", ++lineNumber * lineHeight + dy + "em").text(word);
+      }
+    }
+  });
+}
+
+
 // function to call on the data
 plotData = function(error, data, dataType) {
   if (error) throw error;
+
+  myData = data;
 
   /* Initial Setup */
 
@@ -31,11 +61,11 @@ plotData = function(error, data, dataType) {
     .attr('height', height)
     .attr('width', width);
 
-  var graphMargin = {top: 40, right: width * 0.4, bottom: 20, left: 80},
+  var graphMargin = {top: 40, right: width * 0.4, bottom: 80, left: 80},
     graphWidth = width - graphMargin.left - graphMargin.right,
     graphHeight = height - graphMargin.top - graphMargin.bottom;
 
-  var x = d3.scaleBand().rangeRound([0, graphWidth]).padding(0.1),
+  var x = d3.scaleBand().rangeRound([0, graphWidth]).padding(0.1).paddingInner(0.4),
       y = d3.scaleLinear().rangeRound([graphHeight, 0]);
 
   var axisTextSize = d3.min([d3.max([8, (graphHeight / 20)]), 15]) + "px"
@@ -43,7 +73,7 @@ plotData = function(error, data, dataType) {
   var graph = svg.append("g")
       .attr("transform", "translate(" + graphMargin.left + "," + graphMargin.top + ")");
 
-    x.domain(data.map(function(d) { return d.year; }));
+    x.domain(data.map(function(d) { return d.name; }));
     y.domain([0, 0.16]);
 
   // most recent year of data
@@ -70,13 +100,12 @@ plotData = function(error, data, dataType) {
       .text("");;
 
   /* initialize bars with zero height */
-  // total debt (although only approved debt will be visible)
-  // on mouseover, outlines whole bar.
+  // total obligations (although only interest will be visible)
   graph.selectAll('tBar')
     .data(data)
     .enter().append('rect')
     .attr('class', 'bar tBar')
-    .attr('x', function(d) {return x(d.year)})
+    .attr('x', function(d) {return x(d.name)})
     .attr('y', graphHeight)
     .attr('width', x.bandwidth())
     .attr('height', 0)
@@ -87,21 +116,21 @@ plotData = function(error, data, dataType) {
       floating_tooltip.hideTooltip();
     });
 
-  // bar for unapproved debt. on mouseover, outlines whole bar.
+  // bar for principal
   graph.selectAll(".uBar")
     .data(data)
     .enter().append("rect")
-      .attr("class", "bar uBar")
-      .attr("x", function(d) { return x(d.year); })
-      .attr("y", graphHeight)
-      .attr("width", x.bandwidth())
-      .attr("height", 0)
-      .attr('fill', 'steelblue')
-      .on('mouseenter', showFloatingTooltip)
-      .on('mouseleave', function() {
-        graph.select('.outline').remove();
-        floating_tooltip.hideTooltip();
-      });
+    .attr("class", "bar uBar")
+    .attr("x", function(d) { return x(d.name); })
+    .attr("y", graphHeight)
+    .attr("width", x.bandwidth())
+    .attr("height", 0)
+    .attr('fill', 'steelblue')
+    .on('mouseenter', showFloatingTooltip)
+    .on('mouseleave', function() {
+      graph.select('.outline').remove();
+      floating_tooltip.hideTooltip();
+    });
 
   // make legend
   legend = graph.append('g');
@@ -121,7 +150,7 @@ plotData = function(error, data, dataType) {
     .attr('x', legendX + 1.5 * legendSize)
     .attr('y', legendY + 1.6 * legendSize)
     .attr('font-size', axisTextSize)
-    .text('Voter approved debt');
+    .text('Interest');
 
   // non-voter-approved debt
   legend.append('rect')
@@ -135,130 +164,13 @@ plotData = function(error, data, dataType) {
     .attr('x', legendX + 1.5 * legendSize)
     .attr('y', legendY + 3.1 * legendSize)
     .attr('font-size', axisTextSize)
-    .text('Non-voter approved debt');
+    .text('Principal');
 
-  /* draw debt caps */
-
-  // non-voter approved debt cap
-  var uLine = d3.line()
-      .x(function(d) { return x(d.year) + (0.5 * x.bandwidth()); })
-      .y(function(d) { return y(d.unapprovedDebtCap); });
-
-  graph.append('path')
-    .data([data])
-    .attr('class', 'line uLine')
-    .attr('d', uLine)
-    .attr('stroke-dasharray', '5, 5')
-    .attr('stroke', 'black')
-    .attr('fill', 'none');
-
-  // text for non-voter approved debt cap.
-  // extra work to find the bounding box and make it lighter
-  drawLabel2 = function() {
-    d3.selectAll('#label2').remove();
-
-    myText = graph.append('text')
-      .attr('id', 'label2')
-      .attr('x', x(current_year) + x.bandwidth())
-      .attr('y', y(0.063))
-      .attr('text-anchor', 'end')
-      .attr('fill', 'white')
-      .attr('font-size', axisTextSize)
-      .text('Non-voter approved debt cap');
-
-    var bbox = myText.node().getBBox();
-
-    myRect = graph.append("rect")
-      .attr('id', 'label2')
-      .attr("x", bbox.x)
-      .attr("y", bbox.y)
-      .attr("width", bbox.width)
-      .attr("height", bbox.height)
-      .style("fill", "white")
-      .style("opacity", ".5");
-
-    myText.remove();
-    myText = graph.append('text')
-      .attr('id', 'label2')
-      .attr('x', x(current_year) + x.bandwidth())
-      .attr('y', y(0.063))
-      .attr('text-anchor', 'end')
-      .attr('font-size', axisTextSize)
-      .text('Non-voter approved debt cap');
-  }
-  drawLabel2();
-
-  // total debt cap
-  var tLine = d3.line()
-      .x(function(d) { return x(d.year) + (0.5 * x.bandwidth()); })
-      .y(function(d) { return y(d.totalDebtCap); });
-
-  graph.append('path')
-    .data([data])
-    .attr('class', 'line tLine')
-    .attr('d', tLine)
-    .attr('stroke-dasharray', '2, 2')
-    .attr('stroke', 'black')
-    .attr('fill', 'none');
-
-  graph.append('text')
-    .attr('x', x(current_year) + x.bandwidth())
-    .attr('y', y(0.152))
-    .attr('text-anchor', 'end')
-    .attr('font-size', axisTextSize)
-    .text('Total debt cap');
-
-  // function for toggling to percentage view
-  function drawPercent() {
-    // set the scale
-    var y = d3.scaleLinear().rangeRound([graphHeight, 0]).domain([0, 0.16]);
-
-    // change the axis
-    d3.selectAll('.axis--y')
-        .call(d3.axisLeft(y).ticks(10, "%"));
-
-    d3.selectAll('.yAxisText')
-        .text('');
-
-    // transition the bars
-    graph.selectAll('.tBar')
-      .transition()
-      .duration(500)
-      .attr('y', function(d) {return y(d.totalDebtPct)})
-      .attr('height', function(d) {return graphHeight - y(d.totalDebtPct); });
-
-    // bar for unapproved debt. on mouseover, outlines whole bar.
-    graph.selectAll(".uBar")
-      .transition()
-      .duration(500)
-      .attr("y", function(d) { return y(d.unapprovedDebtPct); })
-      .attr("height", function(d) { return graphHeight - y(d.unapprovedDebtPct); });
-
-    // non-voter approved debt cap
-    var uLine = d3.line()
-        .x(function(d) { return x(d.year) + (0.5 * x.bandwidth()); })
-        .y(function(d) { return y(d.unapprovedDebtCap); });
-
-    d3.selectAll('.uLine')
-      .transition()
-      .duration(500)
-      .attr('d', uLine);
-
-    // total debt cap
-    var tLine = d3.line()
-      .x(function(d) { return x(d.year) + (0.5 * x.bandwidth()); })
-      .y(function(d) { return y(d.totalDebtCap); });
-
-    d3.selectAll('.tLine')
-      .transition()
-      .duration(500)
-      .attr('d', tLine);
-  }
 
   // function for toggling to percentage view
   function drawValues() {
     // set the scale
-    var y = d3.scaleLinear().rangeRound([graphHeight, 0]).domain([0, 900]);
+    var y = d3.scaleLinear().rangeRound([graphHeight, 0]).domain([0, 4500]);
 
     // change the axis
     d3.selectAll('.axis--y')
@@ -271,61 +183,35 @@ plotData = function(error, data, dataType) {
     graph.selectAll('.tBar')
       .transition()
       .duration(500)
-      .attr('y', function(d) {return y(d.totalDebt / 1e6)})
-      .attr('height', function(d) {return graphHeight - y(d.totalDebt / 1e6); });
+      .attr('y', function(d) {return y(d.total / 1e6)})
+      .attr('height', function(d) {return graphHeight - y(d.total / 1e6); });
 
-    // bar for unapproved debt. on mouseover, outlines whole bar.
+    // bar for principal
     graph.selectAll(".uBar")
       .transition()
       .duration(500)
-      .attr("y", function(d) { return y(d.unapprovedDebt / 1e6); })
-      .attr("height", function(d) { return graphHeight - y(d.unapprovedDebt / 1e6); });
-
-    // non-voter approved debt cap
-    var uLine = d3.line()
-        .x(function(d) { return x(d.year) + (0.5 * x.bandwidth()); })
-        .y(function(d) { return y(d.unapprovedDebtCap * d.receipts / 1e6); });
-
-    d3.selectAll('.uLine')
-      .transition()
-      .duration(500)
-      .attr('d', uLine);
-
-    // total debt cap
-    var tLine = d3.line()
-        .x(function(d) { return x(d.year) + (0.5 * x.bandwidth()); })
-        .y(function(d) { return y(d.totalDebtCap * d.receipts / 1e6); });
-
-    d3.selectAll('.tLine')
-      .transition()
-      .duration(500)
-      .attr('d', tLine);
+      .attr("y", function(d) { return y(d.principal / 1e6); })
+      .attr("height", function(d) { return graphHeight - y(d.principal / 1e6); });
 
   }
 
-  // default to drawing percentages
-  drawPercent();
+  // draw the bars
+  drawValues();
+
+
 
   /* tooltip for displaying data on each item */
-  var floating_tooltip = floatingTooltip('floatingTooltip', "315px");
+  var floating_tooltip = floatingTooltip('floatingTooltip', "215px");
 
   function showFloatingTooltip(d) {
-    // get the active view
-    view = d3.select('#view_toolbar').selectAll('.btn').filter('.active').attr('id');
 
-    // set the height
-    if (view=='percentages') {
-      var y = d3.scaleLinear().rangeRound([graphHeight, 0]).domain([0, 0.16]);
-      var h = y(d.totalDebtPct);
-    } else if (view=='values') {
-      var y = d3.scaleLinear().rangeRound([graphHeight, 0]).domain([0, 900]);
-      var h = y(d.totalDebt / 1e6);
-    }
+    var y = d3.scaleLinear().rangeRound([graphHeight, 0]).domain([0, 4500]);
+    var h = y(d.total / 1e6);
 
     // outline the bar for that year
     graph.append('rect')
       .attr('class', 'outline')
-      .attr('x', x(d.year))
+      .attr('x', x(d.name))
       .attr('y', h)
       .attr('width', x.bandwidth())
       .attr('height', graphHeight - h)
@@ -333,49 +219,24 @@ plotData = function(error, data, dataType) {
       .attr('stroke', 'black')
       .attr('stroke-width', 2);
 
-    // redraw the label for the non-voter approved debt cap
-    drawLabel2();
-
-    var content = '<span class="heading"><p style="text-align: center">Fiscal Year ' + d.year + '</p></span>' +
-                  '<table><tr><td style="font-style: italic">Debt service requirements</td><td></td></tr>' + 
-                  '<tr><td style="padding: 0px 10px 0px 20px">Voter approved debt</td><td style="text-align: center">' + formatAmount(d.approvedDebt) + '</td></tr>' +
-                  '<tr><td style="padding: 0px 10px 0px 20px">Non-voter approved debt</td><td style="text-align: center">' + formatAmount(d.unapprovedDebt) + '</td></tr>' + 
-                  '<tr><td style="padding: 0px 10px 0px 20px">Total</td><td style="text-align: center">' + formatAmount(d.totalDebt) + '</td></tr>' + 
-                  '<tr><td style="font-style: italic">General fund receipts</td><td style="text-align: center; font-style: italic">' + formatAmount(d.receipts) + '</td></table>';
+    var content = '<span class="heading"><p style="text-align: center">' + d.name + '</p></span>' +
+                  '<table><tr><td style="padding: 0px 10px 0px 20px">Principal: </td><td style="text-align: center">' + formatAmount(d.principal) + '</td></tr>' +
+                  '<tr><td style="padding: 0px 10px 0px 20px">Interest: </td><td style="text-align: center">' + formatAmount(d.interest) + '</td></tr>' + 
+                  '<tr><td style="padding: 0px 10px 0px 20px">Total: </td><td style="text-align: center">' + formatAmount(d.total) + '</td></tr></table>';
     // display tooltip
     floating_tooltip.revealTooltip(content, d3.event);
   }
 
-  // Sets up the buttons to allow for toggling between modes
-  d3.selectAll('.btn-group')
-    .selectAll('.btn')
-    .on('click', function() {
-        // set all buttons on the clicked toolbar as inactive, then activate the clicked button
-        var p = d3.select(this.parentNode);
-        p.selectAll('.btn').classed('active', false);
-        d3.select(this).classed('active', true);
-  
-        // toggle
-        if (d3.select(this).attr('id')=='percentages') {
-          drawPercent();
-        } else if (d3.select(this).attr('id')=='values') {
-          drawValues();
-        }
-      });
+  // wrap x-axis labels
+  d3.select('.axis--x').selectAll(".tick text")
+    .call(wrap, 100);
 
 }
 
-d3.csv("debt.csv", function(d) {
-  d.year = +d.Fiscal_Year;
-  d.unapprovedDebt = +d.Non_Voter_Approved_Debt;
-  d.approvedDebt = +d.Voter_Approved_Debt;
-  d.totalDebt = +d.Total_Debt;
-  d.receipts = +d.General_Fund_Receipts;
-  d.unapprovedDebtPct = +d.Non_Voter_Approved_Debt_Percent;
-  d.approvedDebtPct = +d.Voter_Approved_Debt_Percent;
-  d.totalDebtPct = +d.Total_Debt_Percent;
-  d.unapprovedDebtCap = +d.Non_Voter_Approved_Debt_Cap;
-  d.totalDebtCap = +d.Total_Debt_Cap;
+d3.csv("outstanding_debt.csv", function(d) {
+  d.name = d.Name;
+  d.principal = d.Principal * 1000;
+  d.interest = d.Interest * 1000;
+  d.total = d.Total * 1000;
   return d;
 }, plotData);
-
