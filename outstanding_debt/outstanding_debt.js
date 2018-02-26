07d623a493238b1b6a04c0c5e401f99d5b021a5d
aescherling <adam.scherling@gmail.com>
2018-02-26 14:54:53 -0800

    outstanding debt chart tentatively complete

--- outstanding_debt/outstanding_debt.js
+++ outstanding_debt/outstanding_debt.js
@@ -1,8 +1,38 @@
 // inspiration from https://bl.ocks.org/mbostock/3885304
 
+var myData;
+
+// function from https://bl.ocks.org/mbostock/7555321 for wrapping text
+function wrap(text, width) {
+  text.each(function() {
+    var text = d3.select(this),
+        words = text.text().split(/\s+/).reverse(),
+        word,
+        line = [],
+        lineNumber = 0,
+        lineHeight = 1.1, // ems
+        y = text.attr("y"),
+        dy = parseFloat(text.attr("dy")),
+        tspan = text.text(null).append("tspan").attr("x", 0).attr("y", y).attr("dy", dy + "em");
+    while (word = words.pop()) {
+      line.push(word);
+      tspan.text(line.join(" "));
+      if (tspan.node().getComputedTextLength() > width) {
+        line.pop();
+        tspan.text(line.join(" "));
+        line = [word];
+        tspan = text.append("tspan").attr("x", 0).attr("y", y).attr("dy", ++lineNumber * lineHeight + dy + "em").text(word);
+      }
+    }
+  });
+}
+
+
 // function to call on the data
 plotData = function(error, data, dataType) {
   if (error) throw error;
+
+  myData = data;
 
   /* Initial Setup */
 
@@ -31,11 +61,11 @@ plotData = function(error, data, dataType) {
     .attr('height', height)
     .attr('width', width);
 
-  var graphMargin = {top: 40, right: width * 0.4, bottom: 20, left: 80},
+  var graphMargin = {top: 40, right: width * 0.4, bottom: 80, left: 80},
     graphWidth = width - graphMargin.left - graphMargin.right,
     graphHeight = height - graphMargin.top - graphMargin.bottom;
 
-  var x = d3.scaleBand().rangeRound([0, graphWidth]).padding(0.1),
+  var x = d3.scaleBand().rangeRound([0, graphWidth]).padding(0.1).paddingInner(0.4),
       y = d3.scaleLinear().rangeRound([graphHeight, 0]);
 
   var axisTextSize = d3.min([d3.max([8, (graphHeight / 20)]), 15]) + "px"
@@ -79,18 +109,28 @@ plotData = function(error, data, dataType) {
     .attr('y', graphHeight)
     .attr('width', x.bandwidth())
     .attr('height', 0)
-    .attr('fill', 'skyblue');
+    .attr('fill', 'skyblue')
+    .on('mouseenter', showFloatingTooltip)
+    .on('mouseleave', function() {
+      graph.select('.outline').remove();
+      floating_tooltip.hideTooltip();
+    });
 
   // bar for principal
   graph.selectAll(".uBar")
     .data(data)
     .enter().append("rect")
-      .attr("class", "bar uBar")
-      .attr("x", function(d) { return x(d.name); })
-      .attr("y", graphHeight)
-      .attr("width", x.bandwidth())
-      .attr("height", 0)
-      .attr('fill', 'steelblue');
+    .attr("class", "bar uBar")
+    .attr("x", function(d) { return x(d.name); })
+    .attr("y", graphHeight)
+    .attr("width", x.bandwidth())
+    .attr("height", 0)
+    .attr('fill', 'steelblue')
+    .on('mouseenter', showFloatingTooltip)
+    .on('mouseleave', function() {
+      graph.select('.outline').remove();
+      floating_tooltip.hideTooltip();
+    });
 
   // make legend
   legend = graph.append('g');
@@ -158,6 +198,39 @@ plotData = function(error, data, dataType) {
   // draw the bars
   drawValues();
 
+
+
+  /* tooltip for displaying data on each item */
+  var floating_tooltip = floatingTooltip('floatingTooltip', "215px");
+
+  function showFloatingTooltip(d) {
+
+    var y = d3.scaleLinear().rangeRound([graphHeight, 0]).domain([0, 4500]);
+    var h = y(d.total / 1e6);
+
+    // outline the bar for that year
+    graph.append('rect')
+      .attr('class', 'outline')
+      .attr('x', x(d.name))
+      .attr('y', h)
+      .attr('width', x.bandwidth())
+      .attr('height', graphHeight - h)
+      .attr('fill', 'none')
+      .attr('stroke', 'black')
+      .attr('stroke-width', 2);
+
+    var content = '<span class="heading"><p style="text-align: center">' + d.name + '</p></span>' +
+                  '<table><tr><td style="padding: 0px 10px 0px 20px">Principal: </td><td style="text-align: center">' + formatAmount(d.principal) + '</td></tr>' +
+                  '<tr><td style="padding: 0px 10px 0px 20px">Interest: </td><td style="text-align: center">' + formatAmount(d.interest) + '</td></tr>' + 
+                  '<tr><td style="padding: 0px 10px 0px 20px">Total: </td><td style="text-align: center">' + formatAmount(d.total) + '</td></tr></table>';
+    // display tooltip
+    floating_tooltip.revealTooltip(content, d3.event);
+  }
+
+  // wrap x-axis labels
+  d3.select('.axis--x').selectAll(".tick text")
+    .call(wrap, 100);
+
 }
 
 d3.csv("outstanding_debt.csv", function(d) {
@@ -167,4 +240,3 @@ d3.csv("outstanding_debt.csv", function(d) {
   d.total = d.Total * 1000;
   return d;
 }, plotData);
-
